f621e093fa50b605af936f33365e860b8739095f
theraghavdua <raghavdua130@gmail.com>
2024-11-23 16:30:00 +0530

    frontend pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyAppointments from './pages/MyAppointments'
+import MyProfile from './pages/MyProfile'
+import Appointment from './pages/Appointment'
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className='mx-4 sm:mx-[10%]'>
+
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/doctors' element={<Doctors />} />
+      <Route path='/doctors/:speciality' element={<Doctors/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/contact' element={<Contact/>} />
+      <Route path='/my-appointments' element={<MyAppointments />} />
+      <Route path='/my-profile' element={<MyProfile />} />
+      <Route path='/appointment/:docId' element={<Appointment />} />
+    </Routes>
     </div>
   )
 }
